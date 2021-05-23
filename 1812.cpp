9981c79ecdbedbd8a0df6f78df2942670ecdd811
Priyank Jain <49121382+PriyankJain10@users.noreply.github.com>
2021-05-23 19:54:16 +0530

    Add files via upload

---
+++ 1812.cpp
@@ -0,0 +1,20 @@
+// time - O(1)   
+// space - O(1)              
+
+#include<bits/stdc++.h>
+using namespace std;
+
+    
+
+    bool squareIsWhite(string coordinates) {
+        int x = (coordinates[0] - 'a') + (coordinates[1] - '0') + 1;
+        return x%2;
+    }
+
+
+
+
+int main(){
+  
+  return 0;
+}
